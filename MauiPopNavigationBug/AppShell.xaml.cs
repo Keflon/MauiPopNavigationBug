6302387e98dbb84884d21bd63b270c375d1b0f41
Keith Pickford <Keith@FunctionZero.com>
2023-03-21 10:45:57 +0000

    Add project files.

---
+++ MauiPopNavigationBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiPopNavigationBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiPopNavigationBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiPopNavigationBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
